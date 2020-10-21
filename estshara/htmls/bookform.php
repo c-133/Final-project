ee37e923f5fd2bcaed7360b42de3def018a0d09d
c-133 <aabul.2002@gmail.com>
2020-10-21 15:00:35 +0300

    done

--- estshara/htmls/bookform.php
+++ estshara/htmls/bookform.php
@@ -25,7 +25,50 @@ include('dbcon.php');
           <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
 
     </head> 
-<body>
+<body onload="myFunction()">
+
+    <div id="loading">
+    <div class="filler"></div>
+    <svg width="200" height="200">
+    
+        <filter id="innerShadow" x="-20%" y="-20%" width="140%" height="140%">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur"/>
+            <feOffset in="blur" dx="2.5" dy="2.5"/>
+        </filter>
+    
+        <g>
+            <circle id="shadow" style="fill:rgba(0,0,0,0.1)" cx="97" cy="100" r="87" filter="url(#innerShadow)"></circle>
+            <circle id="circle" style="stroke: #FFF; stroke-width: 12px; fill:#20B7AF" cx="100" cy="100" r="80"></circle>
+        </g>
+        <g>
+            <line x1="100" y1="100" x2="100" y2="55" transform="rotate(80 100 100)" style="stroke-width: 3px; stroke: #fffbf9;" id="hourhand">
+                <animatetransform attributeName="transform"
+                                  attributeType="XML"
+                                  type="rotate"
+                                  dur="43200s"
+                                  repeatCount="indefinite"/>
+            </line>
+            <line x1="100" y1="100" x2="100" y2="40" style="stroke-width: 4px; stroke: #fdfdfd;" id="minutehand">
+                <animatetransform attributeName="transform"
+                                  attributeType="XML"
+                                  type="rotate"
+                                  dur="3600s"
+                                  repeatCount="indefinite"/>
+            </line>
+            <line x1="100" y1="100" x2="100" y2="30" style="stroke-width: 2px; stroke: #C1EFED;" id="secondhand">
+                <animatetransform attributeName="transform"
+                                  attributeType="XML"
+                                  type="rotate"
+                                  dur="60s"
+                                  repeatCount="indefinite"/>
+            </line>
+        </g>
+        <circle id="center" style="fill:#128A86; stroke: #C1EFED; stroke-width: 2px;" cx="100" cy="100" r="3"></circle>
+    </svg>
+  </div>
+    
+
+ 
 
 
     <!-- Nav Bar -->
@@ -248,6 +291,8 @@ include('dbcon.php');
       </div>
     </footer>
     <!-- end of footer section -->
+      <script src="../js/load.js"></script>  
+      <script src="../js/load.js"></script>  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E=" crossorigin="anonymous"></script>
       <script src="../js/app.js"></script>
